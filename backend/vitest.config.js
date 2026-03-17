f0b206c67c275606a05da18fa084f607b8690dd1
FaisonsLePoint <chaine.faisonslepoint@gmail.com>
2026-03-17 10:00:54 +0100

    add vitest coverage

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -5,7 +5,18 @@ module.exports = defineConfig({
         globals: true,
         environment: 'node',
         pool: 'forks',
-        include: ['test_vitest/****.test.{mjs,js,ts}'],
-        setupFiles: ['dotenv/config']
+        include: ['test_vitest/**/*.test.{mjs,js,ts}'],
+        setupFiles: ['dotenv/config'],
+        coverage: {
+            provider: 'v8',            // ← v8 à la place d'istanbul
+            include: ['api/**/*.js'],
+            exclude: ['api/db.config.js', 'api/config'],
+            thresholds: {
+                statements: 80,
+                branches: 70,
+                functions: 80,
+                lines: 80,
+            }
+        }
     }
 })
