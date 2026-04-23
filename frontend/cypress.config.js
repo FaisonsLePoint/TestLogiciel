0cd17bff3dc1b8724ed3268cc839e1ff7b3cf18e
FaisonsLePoint <chaine.faisonslepoint@gmail.com>
2026-04-23 08:50:34 +0200

    cypress coverage

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,4 +1,10 @@
 import { defineConfig } from "cypress";
+// Pour les anciennes versions cypress
+// import * as ccModule from '@cypress/code-coverage/task.js'
+// const registerCodeCoverageTasks = ccModule.default
+
+
+import coverageTask from '@cypress/code-coverage/task'
 
 export default defineConfig({
   video: true,
@@ -7,6 +13,18 @@ export default defineConfig({
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      // Pour les anciennes versions
+      // registerCodeCoverageTasks(on, config)
+      // return config    
+
+      coverageTask(on, config)
+      return config 
+    },
+  },
+  env: {
+    codeCoverage: {
+      exclude: ['cypress/**/*.*'],
     },
   }
 });
